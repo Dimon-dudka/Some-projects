019bb18ee5c6758cf5eabaa8428b92db724d1a62
Dmitrii <62469554+Dimon-dudka@users.noreply.github.com>
2023-02-12 17:15:45 +0300

    Boarding the plane
    There are n rows in the plane and three seats on the left and right in each row. The extreme seats (A and F) are at the window, the central ones (C and D) are at the aisle. Groups of one, two or three passengers come to check-in. They want to sit side by side, that is, on the same row and on the same side: left or right. For example, a group of two passengers can sit on seats B and C, but cannot sit on seats C and D because they are separated by an aisle, and also cannot sit on seats A and C, because then they will not be next to each other. In addition, one of the passengers in each group is very demanding – he wants to sit either at the window or at the aisle. Of course, each group of passengers wants to take seats in a row with as small a number as possible, because then they will get off the plane more quickly after landing. For each group of passengers, determine whether there are seats on the plane suitable for them.
    The first line contains the number n (1≤n≤100) – the number of rows in the plane. Next, in n lines, the initial seating in the plane is entered in rows (from the first to the nth), where characters. (dot) indicates empty seats, the symbols # (grid) indicate occupied seats, and the symbols _ (underscore) indicate the passage between seats C and D of each row.
    The next line contains the number m (1≤m≤100) - the number of passenger groups. Next in m
     the lines contain descriptions of passenger groups. The description format is as follows: num side position, where num is the number of passengers (number 1, 2 or 3), side is the desired side of the aircraft (left or right line), position is the desired seat of a demanding passenger (aisle or window line).
    If the group can take seats that meet its requirements, then output the string Passengers can take seats: and a list of their seats in the format r o w l e t t e r, ordered in ascending order of the letter of the seat. Then print in n lines the resulting seating on the plane, in the format described above, and the seats occupied by the current group of passengers should be indicated by the symbol X. If the group cannot find seats that meet its requirements, then output the string Cannot fulfill passengers requirements.

---
+++ Boarding_the_plane.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+
+//My C++ settings (using define)
+
+#define PRINT_VECTOR(a) for(const auto&w:a)cout<<w<<" ";
+#define ENTER_DATA_INTO_VECTOR(a) for(auto &w:a)cin>>w;   //Only when we know size of vector
+
+int main() {
+
+		//Aircraft plan:
+		// A | B | C | | D | E | F
+		//1			  _
+		//2			  _
+		//.........................
+
+	
+	int n, m;										//n - row count 
+	cin >> n;
+	vector<vector<char>>proof(n, vector<char>(7));	//2d vector(plan of boarding seats)
+
+	for (size_t i = 0; i < n; ++i) {
+		for (size_t j = 0; j < 7; ++j) {
+			cin >> proof.at(i).at(j);				//Enter the data into this plan with symbols:'.','_','#'
+		}											//where '.' - free seat, '_' - passage, '#' - ocupied seat
+	}
+	
+
+	int num, left, right, pos;
+	string side, position;
+	cin >> m;										//m - number of requests
+
+	for (size_t tmp = 0; tmp < m; ++tmp) {
+
+		cin >> num >> side >> position;				//people count, left or right, desired location
+
+		if (side.at(0) == 'l') {
+
+			if (position.at(0) == 'w') pos = 0;		//by left window
+			else if (position.at(0) == 'a')pos = 2; //by left aisle
+		}
+		else if (side.at(0) == 'r') {
+
+			if (position.at(0) == 'w')pos = 6;		//by right window
+			else if (position.at(0) == 'a')pos = 4; //by right aisle
+		}
+
+		bool flag = false;
+		set<int>three;								//the places for print
+
+		for (size_t i = 0; i < n; ++i) {
+			three.clear();
+
+			if (proof.at(i).at(pos) == '.') {		//if desired seat is free
+				int count = 1;						//count of people that can sit
+
+				if (pos == 0 || pos == 4) {			//if left window or right aisle we gonna check in this way:-->
+					three.insert(pos);				//insert the first free position
+					for (size_t j = 1; j < num; ++j) {
+
+						if (proof.at(i).at(j + pos) == '.') {//proof that the next seat is free
+							++count;
+							three.insert(j + pos);
+						}
+						else break;
+						if (count == num)break;		//if count of people that have a ticket == count 
+													//of people in request --> break
+					}
+
+				}
+				else if (pos == 2 || pos == 6) {   //if right window or left aisle we gonna check in this way:<--
+					three.insert(pos);
+					for (size_t j = 1; j < num; ++j) {
+
+						if (proof.at(i).at(pos - j) == '.') {//proof that the next seat is free
+							++count;
+							three.insert(pos - j);
+						}
+						else break;
+						if (count == num)break;		//if count of people that have a ticket == count 
+													//of people in request --> break
+					}
+				}
+
+				if (count == num) {
+					flag = true;
+					cout << "Passengers can take seats:";
+
+					for (const auto &w:three) {
+						cout<<" " << i + 1;
+						if (w < 3)cout << char(w + 65); //ASCII code
+						else cout << char(w + 64);		//65-1 becouse we have a passage
+						
+						proof.at(i).at(w) = 'X';		//to print theres places on jet plan
+					}
+					cout << endl;
+
+					for (size_t a=0; a < n; ++a) {		//just print jet plan
+						for (size_t b = 0; b < 7; ++b) {
+							cout << proof.at(a).at(b);
+							if (proof.at(a).at(b) == 'X')proof.at(a).at(b) = '#'; //if we are already printed 
+																				  //symbol 'X' it gonna be '#'
+						}
+						cout << endl;
+					}
+
+					break;
+				}
+			}
+
+		}
+		if (flag == false)cout << "Cannot fulfill passengers requirements" << endl; //if there are not places for
+																					//this request
+
+	}
+
+	return 0;
+}
