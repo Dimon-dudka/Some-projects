cadbdc6a7c3d6f404dce0383b1833a4d350886dc
Dmitrii <62469554+Dimon-dudka@users.noreply.github.com>
2023-02-13 13:03:26 +0300

    Update RLE.cpp

--- RLE.cpp
+++ RLE.cpp
@@ -11,23 +11,29 @@ using namespace std;
 #define ENTER_DATA_INTO_VECTOR(a) for(auto &w:a)cin>>w;   //Only when we know size of vector
 //--------------------------------------------------------------------------------------------
 
-template<typename T>						//its works for std::string,std::array or std::vector of char
-void RLE_ENCODING(const T & start) {
+template<typename T>									//its works for std::string,std::array or std::vector of char
+string RLE_ENCODING(const T & start) {
 
-	for (size_t i = 0; i < start.size(); ++i) {		//algorythm is going through all word
+	string rez;									//final RLE encoded word
 
-		int count = 1;					//count of repetitions of letter
+	for (size_t i = 0; i < start.size(); ++i) {		       			//algorythm is going through all word
+
+		int count = 1;								//count of repetitions of letter
 
 		while (i < start.size() - 1 && start.at(i) == start.at(i + 1)) {	//while this letter is repeating
 			++count;								
 			++i;								//increase of i to not proof it in the next time
 		}
 
-		if (count == 1)cout << start.at(i);			//if count == 1 we have to print only the letter without count
-		else cout << start.at(i) << count;
+		if (count == 1)rez.push_back(start.at(i));				//if count == 1 we have to print only the letter without count
+		else {
+			rez.push_back(start.at(i));
+			string tmp = to_string(count);					//becouse some integers may be more then 9
+			rez += tmp;
+		}
 
 	}
-	cout << endl;
+	return rez;
 }
 
 int main() {
@@ -38,8 +44,7 @@ int main() {
 
 	cin >> start;
 	
-	RLE_ENCODING(start);
-	RLE_ENCODING(a);
+	cout<<RLE_ENCODING(start)<<endl;
 	
 	return 0;
 }
