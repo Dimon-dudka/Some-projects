712ffd815a72e4a5e9c595d203d874e0ae914738
Dmitrii <62469554+Dimon-dudka@users.noreply.github.com>
2023-02-13 12:55:14 +0300

    Update RLE.cpp

--- RLE.cpp
+++ RLE.cpp
@@ -11,16 +11,16 @@ using namespace std;
 #define ENTER_DATA_INTO_VECTOR(a) for(auto &w:a)cin>>w;   //Only when we know size of vector
 //--------------------------------------------------------------------------------------------
 
-template<typename T>								//its works for std::string,std::array or std::vector of char
+template<typename T>						//its works for std::string,std::array or std::vector of char
 void RLE_ENCODING(const T & start) {
 
 	for (size_t i = 0; i < start.size(); ++i) {		//algorythm is going through all word
 
-		int count = 1;								//count of repetitions of letter
+		int count = 1;					//count of repetitions of letter
 
 		while (i < start.size() - 1 && start.at(i) == start.at(i + 1)) {	//while this letter is repeating
 			++count;								
-			++i;									//increase of i to not proof it in the next time
+			++i;								//increase of i to not proof it in the next time
 		}
 
 		if (count == 1)cout << start.at(i);			//if count == 1 we have to print only the letter without count
